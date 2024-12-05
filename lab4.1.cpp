d9fae44f3694eb16ccbaadb5fc1c281d0697082d
aayushmak <aayushmakafle@gmail.com>
2024-12-05 08:51:11 +0545

    added files

---
+++ lab4.1.cpp
@@ -0,0 +1,39 @@
+#include <windows.h>  // for MS Windows
+#include <GL\glut.h>  // GLUT, include glu.h and gl.h
+void drawLine();
+void myInit();
+//This is main() function
+int main(int argc, char** argv)
+{
+   	glutInit(&argc, argv);	//initialize the toolkit
+  	 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //set the display mode
+   	glutInitWindowSize(600, 400); //set window size
+   	glutInitWindowPosition(100, 150); //set window position
+   	glutCreateWindow("Drawing a line");	//open the screen window
+   	glutDisplayFunc(drawLine);	//register redraw function
+   	myInit(); 	//additional initializations are necessary
+   	glutMainLoop();	//go into a perpetual loop
+   	return 0;
+}
+
+
+//This function for initialization
+void myInit()
+{
+	glClearColor(1.0, 0.0, 0.0, 0.0); //set red background color
+glColor3f(0.0, 1.0,0.0);	//set the drawing color
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,320,0,240);
+} 
+//This function to draw line
+void drawLine()
+{
+	glClear(GL_COLOR_BUFFER_BIT); //clear the screen
+	glBegin(GL_LINES);
+		glVertex2f(250, 200);
+		glVertex2f(150, 150);
+	glEnd();
+	glFlush();  	//send all output to display
+}
+
---
+++ lab4.5.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>  // for MS Windows
+#include <GL\glut.h>  // GLUT, include glu.h and gl.h
+void drawPolygon();
+void myInit();
+//This is main() function
+int main(int argc, char** argv)
+{
+   	glutInit(&argc, argv);	//initialize the toolkit
+  	 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //set the display mode
+glutInitWindowSize(600, 400); //set window size
+   	glutInitWindowPosition(100, 150); //set window position
+   	glutCreateWindow("Scaling a Polygon");	//open the screen window
+   	glutDisplayFunc(drawPolygon); //register redraw function
+   	myInit(); 	//additional initializations are necessary
+   	glutMainLoop();	//go into a perpetual loop
+   	return 0;
+}
+//This function for initialization
+void myInit()
+{
+	glClearColor(0.0, 1.0, 0.0, 0.0); //set green background color
+	glColor3f(1.0, 0.0,0.0);    //set the drawing color red
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,320,0,240);
+} 
+//This function to translate Polygon
+void drawPolygon()
+{
+	glClear(GL_COLOR_BUFFER_BIT); //clear the screen
+	glTranslatef(50,50,0);   //translate the polgon to tx=50, ty=50
+	glScalef(2.0f, 0.5f, 1.0f);  // Scale by a factor of 2 in the X-axis and 0.5 in the Y-axis 
+	glBegin(GL_POLYGON);
+		glVertex2f(0, 0);
+		glVertex2f(50, 0);
+		glVertex2f(50, 50);
+		glVertex2f(0, 50);
+	glEnd();
+	glFlush();  	//send all output to display
+}
+
